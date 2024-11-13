4b91e2b58af2f8f37a1605e2317d518c4731577f
Kien Le <kien.le2152004@hcmut.edu.vn>
2024-11-13 20:26:15 +0700

    add get print request by printer id

--- src/main/java/com/hcmut/ssps_server/service/interf/IPrintingService.java
+++ src/main/java/com/hcmut/ssps_server/service/interf/IPrintingService.java
@@ -1,7 +1,12 @@
 package com.hcmut.ssps_server.service.interf;
 
 import com.hcmut.ssps_server.model.Document;
+import com.hcmut.ssps_server.model.Printing;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface IPrintingService {
     void addPrintRequest(Document document, int printerId);
+    List<Printing> getPrintRequestsByPrinterId(int printerId, Pageable pageable);
 }
