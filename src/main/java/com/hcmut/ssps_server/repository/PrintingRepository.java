f84f99c73f7578e1385ae3145e093387a3f44007
LamNguyen1407 <ln14072004@gmail.com>
2024-11-18 23:57:59 +0700

    update print request

--- src/main/java/com/hcmut/ssps_server/repository/PrintingRepository.java
+++ src/main/java/com/hcmut/ssps_server/repository/PrintingRepository.java
@@ -23,18 +23,13 @@ public interface PrintingRepository extends JpaRepository<Printing, Long> {
             "    p.id AS printing_id, " +
             "    p.admin_print_mail AS printing_admin_mail, " +
             "    p.expired_time, " +
-            "    p.printer_to_printid, " +
-            "    p.printing_start_time AS printing_start, " +
             "    p.student_upload_mail, " +
             "    d.id AS document_id, " +
             "    d.file_name, " +
-            "    d.file_type, " +
             "    d.number_of_copies, " +
             "    d.page_count, " +
             "    d.paper_size, " +
             "    d.sided_type, " +
-            "    pl.id AS printing_log_id, " +
-            "    pl.admin_print_mail, " +
             "    pl.printer_id, " +
             "    pl.printing_end_time, " +
             "    pl.printing_start_time " +
@@ -57,21 +52,16 @@ public interface PrintingRepository extends JpaRepository<Printing, Long> {
                     ((Number) row[0]).intValue(),                    // p.id
                     (String) row[1],                                // p.admin_print_mail
                     row[2] != null ? ((Timestamp) row[2]).toLocalDateTime().toLocalDate() : null, // p.expired_time
-                    ((Number) row[3]).intValue(),                   // p.printer_to_printid
-                    row[4] != null ? ((Timestamp) row[4]).toLocalDateTime().toLocalDate() : null, // p.printing_start_time
-                    (String) row[5],                                // p.student_upload_mail
-                    ((Number) row[6]).longValue(),                  // d.id
-                    (String) row[7],                                // d.file_name
-                    (String) row[8],                                // d.file_type
-                    ((Number) row[9]).intValue(),                   // d.number_of_copies
-                    ((Number) row[10]).intValue(),                  // d.page_count
-                    (String) row[11],                               // d.paper_size
-                    (String) row[12],                               // d.sided_type
-                    row[13] != null ? ((Number) row[13]).longValue() : null, // pl.id
-                    (String) row[14],                               // pl.admin_print_mail
-                    row[15] != null ? ((Number) row[15]).intValue() : null, // pl.printer_id
-                    row[16] != null ? ((Timestamp) row[16]).toLocalDateTime().toLocalDate() : null, // pl.printing_end_time
-                    row[17] != null ? ((Timestamp) row[17]).toLocalDateTime().toLocalDate() : null  // pl.printing_start_time
+                    (String) row[3],                                // p.student_upload_mail
+                    ((Number) row[4]).longValue(),                  // d.id
+                    (String) row[5],                                // d.file_name
+                    ((Number) row[6]).intValue(),                   // d.number_of_copies
+                    ((Number) row[7]).intValue(),                  // d.page_count
+                    (String) row[8],                               // d.paper_size
+                    (String) row[9],                               // d.sided_type
+                    row[10] != null ? ((Number) row[10]).intValue() : null, // pl.printer_id
+                    row[11] != null ? ((Timestamp) row[11]).toLocalDateTime().toLocalDate() : null, // pl.printing_end_time
+                    row[12] != null ? ((Timestamp) row[12]).toLocalDateTime().toLocalDate() : null  // pl.printing_start_time
             );
             requestPartResponses.add(response);
         }
